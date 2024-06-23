223bacf7cf6b4b94f39e5b84397a50b542ef9346
Spyros Zarkadis <spyroszarkadis@gmail.com>
2024-06-23 08:23:54 +0300

    making part of one round

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 //Getting the computers choice.
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
 
    switch(computerChoice) {
     case 0:
-      computerChoice = "Rock";
+      computerChoice = "rock";
       return ("The computers choice is Rock");
       break;
     
     case 1:
-      computerChoice = "Paper";
+      computerChoice = "paper";
       return ("The computers choice is Paper");
       break;
       
     case 2:
-       computerChoice = "Scissors";
+       computerChoice = "scissors";
        return ("The computers choice is Scissors");
        break;   
    }
@@ -23,7 +23,7 @@ function getComputerChoice() {
 //Getting the humans choice.
 function getHumanChoice() {
    let humanChoice = prompt("Pick up your weapon!"); 
-
+       humanChoice = humanChoice.toLowerCase();
     switch(humanChoice) {
         case "rock":
             humanChoice = "rock";
@@ -40,6 +40,30 @@ function getHumanChoice() {
             return ("Your weapon of choice is Scissors!");
             break;
 
+        default:
+            return prompt("You need to pick one of the three weapons, either Rock, Paper or Scissors.");    
+
     }
 }
 console.log(getHumanChoice());
+
+//making the players score variables.
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+    let result;
+
+    if (computerChoice === humanChoice) {
+        result = "It's a draw";
+    } else if (
+        (humanChoice == "rock" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "rock")
+    ) {
+        result = "The computer wins";
+    } else {
+        result = "The player wins";
+    }
+     console.log(result);
+}
